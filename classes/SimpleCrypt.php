3919af27756aacad56bd30a670c6b48f5ca409b1
Christian Blos <christian.blos@gmx.de>
2015-01-04 20:31:27 +0100

    added SimpleCrypt

---
+++ classes/SimpleCrypt.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Simple encryption with key.
+ *
+ * @author  Christian Blos <christian.blos@gmx.de>
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ * @link    https://github.com/christianblos
+ */
+class SimpleCrypt
+{
+
+    /**
+     * @param mixed  $value
+     * @param string $key
+     *
+     * @return string
+     */
+    public function encode($value, $key)
+    {
+        $result = null;
+        $length = strlen($value);
+        for ($i = 0; $i < $length; $i++) {
+            $char = substr($value, $i, 1);
+            $keychar = substr($key, ($i % strlen($key)) - 1, 1);
+            $char = chr(ord($char) + ord($keychar));
+            $result .= $char;
+        }
+        return base64_encode($result);
+    }
+
+    /**
+     * @param string $value
+     * @param string $key
+     *
+     * @return mixed
+     */
+    public function decode($value, $key)
+    {
+        $result = null;
+        $value = base64_decode($value);
+        $length = strlen($value);
+        for ($i = 0; $i < $length; $i++) {
+            $char = substr($value, $i, 1);
+            $keychar = substr($key, ($i % strlen($key)) - 1, 1);
+            $char = chr(ord($char) - ord($keychar));
+            $result .= $char;
+        }
+        return $result;
+    }
+}
